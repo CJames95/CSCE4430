f0dafec7ab5d9c67196a12948fbcaec7eaefb07c
pearsonpshtoo <64882556+pearsonpshtoo@users.noreply.github.com>
2023-04-26 22:08:59 -0500

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -100,7 +100,7 @@ function App() {
 	style={{ minHeight: '50vh' }}
     >
 	<h>
-		Real Estate Estimator: CSCE 4430
+		Real Estate Estimator: CSCE 4430 - Spring 2023
 	</h>
       <div className={classes.root}>
         <AppBar position="static">
@@ -247,8 +247,16 @@ function App() {
         Created by: Pearson Davenport, Calvin James, Hang Tran, .... 
       </p>
       <p>
-        This project was created for our CSCE 4430: Programming Languages class project for the Spring 2023 semester
+        This project was created for our CSCE 4430: Programming Languages class project for the Spring 2023 semester at The University of North Texas
       </p>
+	<a
+            className="App-link"
+            href="https://github.com/CJames95/CSCE4430"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Link to our public Github repo
+          </a>
          
         </header>
       </div>
